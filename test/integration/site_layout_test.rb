f1e2e3ad583efd85d445822972ed9e9b6cc3712d
李戈 <lige@happysoft.cc>
2016-03-01 16:55:55 +0800

    finish 5 chapter test

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,5 +9,13 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
+    get signup_path
+    assert_select "title", full_title("Sign up")
+  end
+
+  test "signup should has title" do
+    get signup_path
+    assert_response :success
+    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
   end
 end
