d856ca743dbb1705df52ed69a7a38a078680ee2c
李戈 <lige@happysoft.cc>
2016-03-01 16:31:04 +0800

    add first integration test

---
+++ test/integration/site_layout_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+
+class SiteLayoutTest < ActionDispatch::IntegrationTest
+
+  test "layout links" do
+    get root_path
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2 #并不是两次，而是有两个。一个logo，一个导航条
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+  end
+end
